184767c8b54f68da9bcfea88a6d64175b1cb7fa3
Elijah Cruz <ecwebservices@gmail.com>
2021-06-27 03:29:52 -0500

    Added .htaccess, a feature not ready page (in progress, and updated example .env

---
+++ resources/views/dashboard/special/feature-not-ready.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.dashboard')
+
+@section('title', 'Not Ready!')
+
+@section('content')
+    <div class="py-4 mx-auto max-w-7xl">
+        <h1 class="text-3xl md:text-3xl">Whoops!</h1>
+
+        <p>This feature is not yet ready. P
+    </div>
+@endsection
