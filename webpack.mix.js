9d39d59a51b663a421f936da1d2ed36744f166ce
Elijah Cruz <ecwebservices@gmail.com>
2021-06-28 22:44:03 -0500

    Added service worker to webpack

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,6 +1,7 @@
 let mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
+    .js('resources/js/servicesworker.js', 'public/js')
     .setPublicPath('public')
     .postCss("resources/css/app.css", "public/css", [
         require("tailwindcss"),
