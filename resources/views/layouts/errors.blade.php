bd6462326354e6ecc01118fd7efbf0175ec631b5
Elijah Cruz <ecwebservices@gmail.com>
2021-06-28 21:54:54 -0500

    Fixed added space

--- resources/views/layouts/errors.blade.php
+++ resources/views/layouts/errors.blade.php
@@ -11,7 +11,7 @@
         <meta name="theme-color" content="#fefefe">
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <link rel="stylesheet" href="@asset('/css/app .css')" />
+        <link rel="stylesheet" href="@asset('/css/app.css')" />
     </head>
     <body>
         <div class="flex h-screen">
