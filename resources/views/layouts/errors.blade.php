e6a4e6e274710ace43cfec2b66a4fced9b89fb87
Elijah Cruz <ecwebservices@gmail.com>
2021-06-28 21:56:37 -0500

    Added a noindex,nofollow to make sure that search engines dont index WebDis as that could be a security issue

--- resources/views/layouts/errors.blade.php
+++ resources/views/layouts/errors.blade.php
@@ -10,6 +10,7 @@
         <meta name="msapplication-TileColor" content="#da532c">
         <meta name="theme-color" content="#fefefe">
         <meta charset="UTF-8" />
+        <meta name=”robots” content=”noindex,nofollow”>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet" href="@asset('/css/app.css')" />
     </head>
