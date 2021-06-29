6bf40e8b57f623ce4b8b664a319cc619c83d8c81
Elijah Cruz <ecwebservices@gmail.com>
2021-06-29 13:23:19 -0500

    Added an update bash script

---
+++ update.sh
@@ -0,0 +1,31 @@
+echo Updating Webdis...
+
+sleep 1
+
+echo Pulling the new version of Webdis...
+
+# This assumes you installed Webdis Via Git
+git add -A
+git stash
+git pull
+
+echo Reinstalling Composer and NPM dependencies...
+
+# Runs these so we can install our dependencies
+composer install
+npm install
+
+echo Compiling CSS and Javascript...
+
+# If you are in development, remove --production
+npx mix --production
+
+echo Fixing file owning issues...
+
+# Runs these so our files are owned correctly be the correct user and group, and fixes the storage and bootstrap issues
+sudo chown -R $USER:www-data bootstrap/ # If your webserver doesn't use www-data, change this.
+sudo chown -R $USER:www-data storage/
+chmod -R 775 bootstrap/
+chmod -R 775 storage/
+
+echo Webdis has been updated!
