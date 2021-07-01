ea4454cff57bca7982bf33d06bca638f38928293
Elijah Cruz <ecwebservices@gmail.com>
2021-06-30 19:12:39 -0500

    Updated update.sh

--- update.sh
+++ update.sh
@@ -9,8 +9,9 @@ export WEBDIS_CURRENT_VERSION="0.0.3"
 echo Pulling the new version of Webdis...
 
 # This assumes you installed Webdis Via Git
-#git add -A
-#git stash
-#git pull
+git add -A
+git stash
+git stash clear
+git pull
 
 ./src/Foundation/update.sh
